a9799457de66e03de9ece56a7c7a5e44e05cc56c
Shivaram <p.k.shivaram@outlook.in>
2023-01-24 21:59:21 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+    default = "E:/terraform/pet.txt"  
+}
+variable "content" {
+    default = "I Love pet"  
+}
+variable "prefix" {
+    default = "Mrs"  
+}
+variable "separator" {
+    default = ","  
+}
+variable "length" {
+    default = "5"  
+}
